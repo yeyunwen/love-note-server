189ab94dd7388a60d7aa56c3f115f94695bf7397
yeyunwen <1571374338@qq.com>
2024-12-24 23:41:35 +0800

    refactor(upload): 注释multer对文件名中文编码处理的问题

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -10,12 +10,16 @@ import { Image } from '~/note/entities/image.entity';
     MulterModule.register({
       fileFilter: (req, file, callback) => {
         try {
-          if (file.originalname) {
-            file.originalname = Buffer.from(
-              file.originalname,
-              'binary',
-            ).toString('utf8');
-          }
+          /**
+           * 以下代码是用于解决multer对文件名中文编码处理的问题
+           * 代码被注释掉了，因为文件名中文编码问题似乎已经被解决了
+           */
+          // if (file.originalname) {
+          // file.originalname = Buffer.from(
+          //   file.originalname,
+          //   'binary',
+          // ).toString('utf8');
+          // }
           callback(null, true);
         } catch (err) {
           callback(new Error('文件名编码错误'), false);
